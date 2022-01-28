93aa609661bbb9ca36983056abb21f273d20770b
Qeswer <56409028+Qeswer@users.noreply.github.com>
2022-01-29 00:36:29 +0500

    Update exel_2.py

--- exel/exel_2.py
+++ exel/exel_2.py
@@ -17,6 +17,13 @@ def y_list(x, col):  # функция вытаскивания значений 
     return y_list_[1:len(y_list_)]
 
 
+def name_sens(x):
+    sensors_name = []
+    for i in range(2, x.max_column):
+        sensors_name.append(x.cell(row=1, column=i).value)
+    return sensors_name
+
+
 def reg(x):  # функция регулярного выражения
     list1 = []
     list2 = []
@@ -42,7 +49,7 @@ def plot_sens(T: list, S: list) -> list:
     :rtype: list
     """
     for i in range(len(S)):
-        plt.plot(T, S[i])
+        plt.plot(T, S[i], label=f'{name_sens(book2)[i]}')
     plt.xlabel("time", fontsize=14)
     plt.ylabel("value " + r'$\lambda$' + ", Нм")
     plt.title("Изменение длины волны со временем")
@@ -50,7 +57,7 @@ def plot_sens(T: list, S: list) -> list:
     ax.tick_params(which="major",  # применяем основным делениям
                    length=5,  # длина делений
                    width=0.5,
-                   pad=10,  # Расстояние между черточкой и ее подписью
+                   pad=5,  # Расстояние между черточкой и ее подписью
                    labelsize=8,  # Размер подписи
                    bottom=True,
                    labelrotation=45)  # Поворот подписей
@@ -59,6 +66,7 @@ def plot_sens(T: list, S: list) -> list:
     plt.grid(color='k',
              linewidth='0.5',
              linestyle='--')
+    plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
     plt.show()
 
 
